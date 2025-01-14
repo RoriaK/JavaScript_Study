6b4c91ce58b9fd3a73e22d7e300796ca349a93e7
RoriaK <cutesm4041@tukorea.ac.kr>
2025-01-14 13:42:01 +0900

    함수 3가지

--- test.js
+++ test.js
@@ -1,37 +1,19 @@
-//새 배열 안에 사람 이름만 깔끔하게 모아보자!
-let info = 
-    [
-        {
-            "name": "김철수",
-            "age": 30,
-            "city": "서울"
-        },
-        {
-            "name": "박영희",
-            "age": 25,
-            "city": "부산"
-        },
-        {
-            "name": "이민수",
-            "age": 35,
-            "city": "대구"
-        },
-        {
-            "name": "최은영",
-            "age": 28,
-            "city": "인천"
-        }
-    ]
-
-
-//배열 이름 하나를 파둔다. (저장할 공간 마련)
-let names = [];
-for(let i of info){
-    names.push(i.name)
+function add(n1,n2){ //선언식1
+    return n1 + n2;
 }
 
-console.log(names)
+let sub = function (n3,n4){ //이름이 적혀지지 않는 함수 // = 람다 //선언식2
+    return n3-n4
+}
 
-// let names = info[0].map(person => person.name);
+//console.log(sub(3,4))
 
-// console.log(names);
+//let sub2 = (n5,n6) => {return n5 - n6}
+let sub2 = (n5,n6) => n5 - n6 //return도 쓰지 말고, 중 괄호도 쓰지 말자
+console.log(sub2(5,8))
+
+let sub3 = n3 => n3+1 //인자가 하나라면 괄호 없애고, 인자를 하나만 넣을 수 있음
+console.log(sub3(1))
+
+let sub4 = p1 => p1**2
+console.log(sub4(5))
