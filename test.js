8699e739ba0f0a6984dc6e8b9ccb17818652091e
RoriaK <cutesm4041@tukorea.ac.kr>
2025-01-14 14:15:06 +0900

    함수 3가지2

--- test.js
+++ test.js
@@ -1,19 +1,26 @@
-function add(n1,n2){ //선언식1
-    return n1 + n2;
-}
+const person3 = {
+    name: '철수',
+    greet: () => console.log('안녕하세요. 반갑습니다.'),
+    great4(){
+        console.log('다른인사')
+    }
+};
 
-let sub = function (n3,n4){ //이름이 적혀지지 않는 함수 // = 람다 //선언식2
-    return n3-n4
-}
+//2번
+const person = {
+    name: '철수',
+    greet: () => console.log('안녕하세요. 반갑습니다.');
+    
+};
 
-//console.log(sub(3,4))
+person.greet(); // 안녕하세요. 반갑습니다.
 
-//let sub2 = (n5,n6) => {return n5 - n6}
-let sub2 = (n5,n6) => n5 - n6 //return도 쓰지 말고, 중 괄호도 쓰지 말자
-console.log(sub2(5,8))
+//1번
+const person2 = {
+    name: '철수',
+    greet: function () {
+        console.log('안녕하세요. 반갑습니다.');
+    },
+};
 
-let sub3 = n3 => n3+1 //인자가 하나라면 괄호 없애고, 인자를 하나만 넣을 수 있음
-console.log(sub3(1))
-
-let sub4 = p1 => p1**2
-console.log(sub4(5))
+person2.greet(); // 안녕하세요. 반갑습니다.
